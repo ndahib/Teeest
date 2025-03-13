3938d279f1e2b0c4cbecb76b96fcb53b6e71ad42
ndahib <dahibnassima64@gmail.com>
2025-03-13 20:44:35 +0000

    Custimizing Header

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,25 +1,35 @@
-import { TentTree } from "lucide-react";
+import { Menu, TentTree, X } from "lucide-react";
+import { useState } from "preact/hooks";
 
 
 export default function Header(){
+    const [showMenu, setShowMenu] = useState(false)
     return (
-        <header className="flex flex-row">
+        <header className="flex flex-row  items-center justify-between sm:justify-around p-2 text-white">
             <div className="flex">
-                <TentTree />
-                <span>Tree9Morocco</span>
+                <TentTree className="text-white"/>
+                <a href="/" className="text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-yellow-500">Tree9Morocco</a>
             </div>
-            <div className="flex justify-between">
-                <span>
-                    About
-                </span>
-                <span>
-                    Discover
-                </span>
-                <span>
-                    Events
-                </span>
-            </div>
+            <nav className="hidden sm:flex justify-between items-center gap-4 font-semibold">
+                <a href="#">About</a>
+                <a href="#">Discover</a>
+                <a href="#">Events</a>
+            </nav>
             <button>Sign In</button>
+            <nav className="flex sm:hidden flex-col items-end gap-4 font-semibold">
+                <button onClick={() => setShowMenu(!showMenu)}>
+                    {!showMenu ? <Menu/> : <X/>}
+                </button>
+                {showMenu ? (
+                    <>
+                        <a href="#">About</a>
+                        <a href="#">Discover</a>
+                        <a href="#">Events</a>
+                    </>
+                ) : (
+                    <></>
+                )}
+            </nav>
         </header>
     )
 }
